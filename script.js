dc726bbfc852e008263b1ba11826495833305686
Kaiki Alvarenga <kaikialvarengasouza098@gmail.com>
2024-01-26 22:05:34 -0300

    Finalization of menu and new-appointment button

---
+++ script.js
@@ -0,0 +1,25 @@
+const newAppointmentButton = document.getElementById('new-appointment');
+const newAppointmentOptions = document.querySelectorAll('.menu-new-appointment__option');
+
+const darkLayer = document.createElement('div');
+darkLayer.style.position = 'fixed';
+darkLayer.style.top = '0';
+darkLayer.style.left = '0';
+darkLayer.style.width = '100%';
+darkLayer.style.height = '100%';
+darkLayer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+
+
+newAppointmentButton.addEventListener('click', () => {
+    newAppointmentOptions.forEach(element => {
+        element.classList.toggle('hidden');
+    });
+
+    const existingDarkLayer = document.body.contains(darkLayer);
+
+    if (existingDarkLayer) {
+        document.body.removeChild(darkLayer);
+    } else {
+        document.body.appendChild(darkLayer);
+    }
+});
